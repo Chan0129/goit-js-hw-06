261bc11cd015fc143dc9da2091155939eb90369b
Chan0129 <142906874+Chan0129@users.noreply.github.com>
2024-02-07 17:53:15 +0800

    Task 8 change to arrow function

--- js/task-08.js
+++ js/task-08.js
@@ -1,21 +1,19 @@
-document
-  .querySelector(".login-form")
-  .addEventListener("submit", function (event) {
-    event.preventDefault();
+document.querySelector(".login-form").addEventListener("submit", (event) => {
+  event.preventDefault();
 
-    const form = event.target;
-    const email = form.elements.email.value.trim();
-    const password = form.elements.password.value.trim();
+  const form = event.target;
+  const email = form.elements.email.value.trim();
+  const password = form.elements.password.value.trim();
 
-    if (!email || !password) {
-      alert("Please fill in the fields.");
-      return;
-    }
-    const formData = {
-      email: email,
-      password: password,
-    };
-    console.log(formData);
+  if (!email || !password) {
+    alert("Please fill in the fields.");
+    return;
+  }
+  const formData = {
+    email,
+    password,
+  };
+  console.log(formData);
 
-    form.reset();
-  });
+  form.reset();
+});
