08a7f0257811e95e65663aa6cf177d1390a62f8c
Chan0129 <142906874+Chan0129@users.noreply.github.com>
2024-02-07 17:58:20 +0800

    Update task 7

--- js/task-07.js
+++ js/task-07.js
@@ -1,9 +1,9 @@
 const fontSizeControl = document.getElementById("font-size-control");
 const text = document.getElementById("text");
 
-function updateFontSize() {
+const updateFontSize = () => {
   const newSize = fontSizeControl.value + "px";
   text.style.fontSize = newSize;
-}
-fontSizeControl.addEventListener("input", updateFontSize);
+};
 
+fontSizeControl.addEventListener("input", updateFontSize);
